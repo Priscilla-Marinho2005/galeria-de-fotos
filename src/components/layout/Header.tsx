086d6032c1bc6a5a27816b4cf217c4a006ec1a9e
PRISCILLA MARINHO <priscillamarinho2005@gmail.com>
2026-01-07 14:35:10 -0300

    Header and fonts

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,8 +2,13 @@
 function Header() {
 
      return(
-          <div>
-               Header
+          <div className="
+               poppins text-center
+               text-white bg-linear-to-br from-[#8B5CF6] to-[#C084FC]
+               py-11
+          ">
+               <h1 className="font-medium text-5xl">Galeria de Fotos</h1>
+               <p className="font-light mt-2">Explore nossa coleção de imagens</p>   
           </div>
      )
 }
