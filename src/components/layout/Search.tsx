f312a0952f5df05e64b1308e829ad1ea7b1852b5
PRISCILLA MARINHO <priscillamarinho2005@gmail.com>
2026-01-07 14:42:22 -0300

    Installation  lucide icons

--- src/components/layout/Search.tsx
+++ src/components/layout/Search.tsx
@@ -2,8 +2,13 @@
 function Search() {
 
      return(
-          <div>
-               Search
+          <div className="flex justify-center my-5 mx-[300px] max-w-full">
+               <div className="border rounded-md p-2 w-full">
+                    <p>Pesquise o nome da imagem...</p>
+                    <div>
+                         
+                    </div>
+               </div>
           </div>
      )
 }
