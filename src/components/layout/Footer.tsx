24ccda4cc9c13c69683011d66238face8b7b3e89
PRISCILLA MARINHO <priscillamarinho2005@gmail.com>
2026-01-07 16:22:45 -0300

    Footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,8 +2,8 @@
 function Footer() {
 
      return(
-          <div>
-               Footer
+          <div className="border-t border-gray-300 text-center p-5 text-[#7C3AED]">
+               <p>&copy; {new Date().getFullYear()} Minha Galeria de Fotos. Todos os direitos reservados</p>
           </div>
      )
 }
