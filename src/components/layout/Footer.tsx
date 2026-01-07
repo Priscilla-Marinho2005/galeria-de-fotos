5f8f060fea4ee669a3b15b58a5e4d1feebe5cb6c
PRISCILLA MARINHO <priscillamarinho2005@gmail.com>
2026-01-07 17:41:39 -0300

    Font change

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@
 function Footer() {
 
      return(
-          <div className="border-t border-gray-300 text-center p-5 text-[#7C3AED]">
+          <div className="poppins border-t border-gray-300 text-center p-5 text-[#8B5CF6]">
                <p>&copy; {new Date().getFullYear()} Minha Galeria de Fotos. Todos os direitos reservados</p>
           </div>
      )
